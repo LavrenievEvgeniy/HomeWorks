ea0df5ab20deca50aec21b441f3872a7b81b617c
LavrentievEvgeniy <megolavrik@gmail.com>
2022-05-17 16:36:48 +0300

    Дз к уроку №2

--- src/main/java/ru/geekbrains/homeworks/Lesson2.java
+++ src/main/java/ru/geekbrains/homeworks/Lesson2.java
@@ -2,7 +2,7 @@ package ru.geekbrains.homeworks;
 
 public class Lesson2 {
     public static void main(String[] args) {
-       boolean d = checkFrom10to20(6,8);
+        boolean d = checkFrom10to20(6, 8);
         System.out.println(d);
 
         positiveOrNegative(2);
@@ -10,16 +10,16 @@ public class Lesson2 {
         boolean v = trueOrFalse(10);
         System.out.println(v);
 
-        printStringNTimes();
+        printStringNTimes("Повтори строку", 7);
     }
 
     private static boolean trueOrFalse(int i) {
         return i <= 0;
-        }
+    }
 
 
     private static void positiveOrNegative(int i) {
-        if(i >= 0){
+        if (i >= 0) {
             System.out.println("Число положительное");
         } else System.out.println("Число отрицательное");
 
@@ -28,16 +28,15 @@ public class Lesson2 {
     public static boolean checkFrom10to20(int a, int b) {
         int c = a + b;
         return c >= 10 && c <= 20;
-        }
+    }
 
-    public static void printStringNTimes (String x, int f){
-        int x = "Privet";
-        for (int i = 0; i < 6; i++) {
-            System.out.println(i);
-        }
-            
+    public static void printStringNTimes(String string, int f) {
+        for (int i = 0; i < f; i++) {
+            System.out.println(string);
         }
 
     }
+
+}
 
 
